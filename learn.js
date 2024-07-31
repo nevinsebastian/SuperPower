6290bf9b45c223e625a85dcb4f7369b7c127545b
nevinsebastian <nevinsebastian@cse.carmelcet.in>
2024-07-31 11:58:26 +0530

    datatypes

--- learn.js
+++ learn.js
@@ -6,3 +6,16 @@
     wwith "/*"
     
     */
+
+    /* data types:
+    undifined , null , boolean , string , symbol, number , and object
+
+    */
+
+    var myName = "Nevin" // var can be used throught the program
+
+    myName = 9
+
+    let ourName = "Gluping" //will only be used within the scop 
+
+    const pi = 3.14 // it can never change
